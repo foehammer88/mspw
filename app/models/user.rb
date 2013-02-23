f90876cf3680fa09c1677c303f310dc08af4fdb3
Patrick Ganson <foehammer88@gmail.com>
2013-02-23 17:57:02 -0500

    Add users table

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  attr_accessible :address, :college, :email, :firstname, :lastname, :password, :password_confirmation
+
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                          uniqueness: { case_sensitive: false }
+   validates :password, presence: true, length: { minimum: 6 }
+   validates :password_confirmation, presence: true
+end

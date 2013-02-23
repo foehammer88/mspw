8476dddac3d7bea78cbad3208c616b2a48f2c495
Patrick Ganson <foehammer88@gmail.com>
2013-02-23 12:12:15 -0500

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myspiderweb::Application.config.session_store :cookie_store, key: '_myspiderweb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myspiderweb::Application.config.session_store :active_record_store

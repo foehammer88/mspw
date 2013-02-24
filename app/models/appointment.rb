08905ec80fa5914e5f21e66eec415301aaab7184
Patrick Ganson <foehammer88@gmail.com>
2013-02-23 21:55:53 -0500

    Revert to 7c7ec0e76

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,8 +1,6 @@
 class Appointment < ActiveRecord::Base
   attr_accessible :completed, :date, :username, :verified
 
-  belongs_to :user
-
   #validates :id, presence: true,
   #                    uniqueness: true
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,3 @@
 class Game < ActiveRecord::Base
   attr_accessible :college, :date, :level, :money, :number_applications, :points, :username
-
-  belongs_to :user
-
-  validates :username, presence: true,
-                                uniqueness: true
-
-
 end

67fc6be8f433ab278ea0d5f6f42ed0b1d8aeddc6
Brad Corry <bmc4166@gmail.com>
2013-02-24 03:51:20 -0500

    update home and donate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130224012134) do
+ActiveRecord::Schema.define(:version => 20130224011256) do
 
   create_table "analytics", :force => true do |t|
     t.string   "college"
